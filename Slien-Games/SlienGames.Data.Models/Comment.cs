7ae1aa3c84d47255a2e3f8b7a40e022772d2cddf
sguzunov <sguzunov@gmail.com>
2017-01-31 02:11:26 +0200

    Added Comment, GameProfile, Image, LikeHate db models.

---
+++ Slien-Games/SlienGames.Data.Models/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using SlienGames.Data.Models.Contracts;
+
+namespace SlienGames.Data.Models
+{
+    public class Comment : IDbModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(100)]
+        public string Content { get; set; }
+
+        /* TODO: Author --> User */
+
+        public DateTime PostedOn { get; set; }
+
+        public int GameProfileId { get; set; }
+
+        public GameProfile GameProfile { get; set; }
+    }
+}
---
+++ Slien-Games/SlienGames.Data.Models/Image.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using SlienGames.Data.Models.Contracts;
+
+namespace SlienGames.Data.Models
+{
+    public class Image : IDbModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Extension { get; set; }
+    }
+}
