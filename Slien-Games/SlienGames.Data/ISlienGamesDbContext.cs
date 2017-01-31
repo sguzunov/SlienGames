8b4f635386982a7a4a55cfdad388e077eca2ebc7
sguzunov <sguzunov@gmail.com>
2017-01-31 03:08:49 +0200

    Add application db context.

---
+++ Slien-Games/SlienGames.Data/ISlienGamesDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+using SlienGames.Data.Models;
+
+namespace SlienGames.Data
+{
+    public interface ISlienGamesDbContext
+    {
+        IDbSet<Comment> Comments { get; set; }
+
+        IDbSet<GameProfile> GamesProfiles { get; set; }
+
+        IDbSet<Image> Images { get; set; }
+
+        IDbSet<Vote> Votes { get; set; }
+
+        IDbSet<T> Set<T>() where T : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+    }
+}
