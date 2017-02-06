204683b844750cf60b4fb3e99e35629d665feab2
gyankov <g.yankov951@abv.bg>
2017-02-06 16:54:25 +0200

    Added tests

---
+++ Slien-Games/SlienGames.Tests/SlienGames.Data.Models.Tests/CommentTests/ConstructorShould.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using SlienGames.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlienGames.Tests.SlienGames.Data.Models.Tests.CommentTests
+{
+    [TestFixture]
+   public class ConstructorShould
+    {
+        [Test]
+        public void HaveParametlessConstructor()
+        {
+            var type = typeof(Comment);
+
+            var ctorParameters = new Type[] { };
+            var bindingFlags = BindingFlags.Public | BindingFlags.Instance;
+            var ctor = type.GetConstructor(bindingFlags, null, ctorParameters, null);
+
+            Assert.That(ctor, Is.Not.Null);
+        }
+    }
+}
