9fee25b27ef9faa5b42fbba4592606d24b1b35e2
gyankov <g.yankov951@abv.bg>
2017-02-06 16:49:34 +0200

    Vote tested

---
+++ Slien-Games/SlienGames.Tests/SlienGames.Data.Models.Tests/VoteTests/ConstructorShould.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using SlienGames.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlienGames.Tests.SlienGames.Data.Models.Tests.VoteTests
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void HaveParametlessConstructor()
+        {
+            var type = typeof(Vote);
+
+            var ctorParameters = new Type[] { };
+            var bindingFlags = BindingFlags.Public | BindingFlags.Instance;
+            var ctor = type.GetConstructor(bindingFlags, null, ctorParameters, null);
+
+            Assert.That(ctor, Is.Not.Null);
+        }
+        [Test]
+        public void InitializeVotesCorrectly()
+        {
+            var vote = new Vote();
+
+            var games = vote.Games;
+
+            Assert.That(games, Is.Not.Null.And.InstanceOf<HashSet<GameProfile>>());
+        }
+    }
+}
---
+++ Slien-Games/SlienGames.Tests/SlienGames.Data.Models.Tests/VoteTests/ValueShould.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using SlienGames.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlienGames.Tests.SlienGames.Data.Models.Tests.VoteTests
+{
+    [TestFixture]
+    class ValueShould
+    {
+        [Test]
+        public void Be_TypeOfBool()
+        {
+            var vote = new Vote();
+            vote.Value = true;
+
+            var result = vote.Value.GetType();
+
+            Assert.True(result == typeof(bool));
+        }
+
+        [Test]
+        public void GetAndSet_ShouldBePublic()
+        {
+            var vote = new Vote();
+            vote.Value = true;
+
+            Assert.True(vote.Value);
+        }
+    }
+}
