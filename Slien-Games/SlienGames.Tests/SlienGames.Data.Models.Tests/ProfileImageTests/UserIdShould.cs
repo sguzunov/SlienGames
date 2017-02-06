bcfa52e0a76a8a6e2f1180dadbecbe4e96b6aa47
gyankov <g.yankov951@abv.bg>
2017-02-06 16:42:25 +0200

    ProfileImage tested

---
+++ Slien-Games/SlienGames.Tests/SlienGames.Data.Models.Tests/ProfileImageTests/UserIdShould.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using SlienGames.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlienGames.Tests.SlienGames.Data.Models.Tests.ProfileImageTests
+{
+    [TestFixture]
+    public class UserIdShould
+    {
+        [Test]
+        public void Be_TypeOfInt()
+        {
+            var profileImage = new ProfileImage();
+            profileImage.UserId = 1;
+
+            var result = profileImage.Id.GetType();
+
+            Assert.True(result == typeof(int));
+        }
+
+        [Test]
+        public void GetAndSet_ShouldBePublic()
+        {
+            var profileImage = new ProfileImage();
+
+            profileImage.UserId = 1;
+
+            Assert.True(profileImage.UserId == 1);
+        }
+    }
+}
---
+++ Slien-Games/SlienGames.Tests/SlienGames.Data.Models.Tests/ProfileImageTests/UserShould.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using SlienGames.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlienGames.Tests.SlienGames.Data.Models.Tests.ProfileImageTests
+{
+    [TestFixture]
+    public class UserShould
+    {
+        [Test]
+        public void Be_TypeOfCoverImage()
+        {
+            var profileImage = new ProfileImage();
+            var user = new User();
+            profileImage.User = user;
+
+            var result = profileImage.User.GetType();
+
+            Assert.True(result == typeof(User));
+        }
+
+        [Test]
+        public void GetAndSeT_ShouldBePublic()
+        {
+            var profileImage = new ProfileImage();
+            var user = new User();
+
+            profileImage.User = user;
+
+            Assert.True(profileImage.User == user);
+        }
+    }
+}
