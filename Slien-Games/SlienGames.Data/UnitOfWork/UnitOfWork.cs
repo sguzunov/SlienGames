c93d68abbb53e5d195d70a88a1247f1965fc8a88
gyankov <g.yankov951@abv.bg>
2017-02-07 11:01:43 +0200

    Added UnitOfWork

---
+++ Slien-Games/SlienGames.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using SlienGames.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlienGames.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private SlienGamesDbContext dbContext;
+
+        public UnitOfWork(SlienGamesDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public void Commit()
+        {
+            this.dbContext.SaveChanges();
+        }
+    }
+}
