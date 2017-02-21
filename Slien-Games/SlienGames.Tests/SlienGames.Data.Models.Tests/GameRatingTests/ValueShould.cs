22f4b4b27def2c72d525b1354b607829531fa0da
sguzunov <sguzunov@gmail.com>
2017-02-21 12:47:02 +0200

    Fixed models test to test GameRating..

---
+++ Slien-Games/SlienGames.Tests/SlienGames.Data.Models.Tests/GameRatingTests/ValueShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+using SlienGames.Data.Models;
+
+namespace SlienGames.Tests.SlienGames.Data.Models.Tests.VoteTests
+{
+    [TestFixture]
+    class ValueShould
+    {
+        [Test]
+        public void Be_TypeOfInt()
+        {
+            var vote = new GameRating();
+            vote.Value = 1;
+
+            var result = vote.Value.GetType();
+
+            Assert.True(result == typeof(int));
+        }
+    }
+}
